e35c2de41a49e32aab9382d454c41915314f3781
Mathieu Virbel <mat@kivy.org>
2019-02-01 10:53:44 +0100

    update example to generate from time to time fake exception to test stdout+stderr stream

--- examples/simple.py
+++ examples/simple.py
@@ -6,4 +6,9 @@ var = 0
 while True:
     var += 1
     print("var is {}".format(var))
-    time.sleep(1)
+    time.sleep(0.5)
+    if var % 10 == 0:
+        try:
+            raise Exception("Oh noes!")
+        except Exception as e:
+            import traceback; traceback.print_exc()
